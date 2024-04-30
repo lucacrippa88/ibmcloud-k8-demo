fc1a5760a65d15f6b78555302518e68db7c0b19d
Massimo Caprinali <massimo.caprinali@it.ibm.com>
2024-04-30 15:49:11 +0200

    remove cos

--- main.tf
+++ main.tf
@@ -88,19 +88,19 @@ resource "ibm_container_vpc_cluster" "cluster" {
 #   }
 # }
 
-resource "ibm_resource_instance" "cos_instance" {
-  name     = var.service_instance_name
-  service  = "cloud-object-storage"
-  plan     = "standard"
-  location = "global"
-}
+# resource "ibm_resource_instance" "cos_instance" {
+#   name     = var.service_instance_name
+#   service  = "cloud-object-storage"
+#   plan     = "standard"
+#   location = "global"
+# }
 
-resource "ibm_container_bind_service" "bind_service" {
-  cluster_name_id     = ibm_container_vpc_cluster.cluster.id
-  service_instance_id = element(split(":", ibm_resource_instance.cos_instance.id), 7)
-  namespace_id        = "default"
-  role                = "Writer"
-}
+# resource "ibm_container_bind_service" "bind_service" {
+#   cluster_name_id     = ibm_container_vpc_cluster.cluster.id
+#   service_instance_id = element(split(":", ibm_resource_instance.cos_instance.id), 7)
+#   namespace_id        = "default"
+#   role                = "Writer"
+# }
 
 data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id = ibm_container_vpc_cluster.cluster.id
