70e627456b156b83bde1a975962beabd10814f3e
Luca Crippa <luca.crippa88@gmail.com>
2024-04-30 16:17:20 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -16,7 +16,7 @@ output "ibm_cr_namespace" {
 }
 // This allows cr_retention_policy data to be referenced by other resources and the terraform CLI
 // Modify this if only certain data should be exposed
-output "ibm_cr_retention_policy" {
-  value       = ibm_cr_retention_policy.cr_retention_policy_instance
-  description = "cr_retention_policy resource instance"
-}
+#output "ibm_cr_retention_policy" {
+#  value       = ibm_cr_retention_policy.cr_retention_policy_instance
+#  description = "cr_retention_policy resource instance"
+#}
