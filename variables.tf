32bd488a34f32a6f916d728cf88e22841f0aef80
Luca Crippa <luca.crippa88@gmail.com>
2024-05-01 12:05:33 +0200

    Remove retention policy

--- variables.tf
+++ variables.tf
@@ -62,20 +62,3 @@ variable "cr_namespace_tags" {
   type        = set(string)
   default     = []
 }
-
-// Resource arguments for cr_retention_policy
-#variable "cr_retention_policy_namespace" {
- # description = "The namespace to which the retention policy is attached."
- # type        = string
- # default     = "birds"
-#}
-#variable "cr_retention_policy_images_per_repo" {
- # description = "Determines how many images will be retained for each repository when the retention policy is executed. The value -1 denotes 'Unlimited' (all images are retained)."
- # type        = number
- # default     = 10
-#}
-#variable "cr_retention_policy_retain_untagged" {
-#  description = "Determines if untagged images are retained when executing the retention policy. This is false by default meaning untagged images will be deleted when the policy is executed."
-#  type        = bool
-#  default     = false
-#}
